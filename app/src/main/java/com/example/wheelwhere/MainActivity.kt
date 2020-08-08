57d6db56cb4f5a971ddbd5d279aeab0b878240f9
i-Pad <omg0207@gmail.com>
2020-08-08 22:15:26 +0900

    data

--- app/src/main/java/com/example/wheelwhere/MainActivity.kt
+++ app/src/main/java/com/example/wheelwhere/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.wheelwhere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
