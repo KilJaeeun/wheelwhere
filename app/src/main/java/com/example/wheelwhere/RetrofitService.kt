c49d63369de0dc72851df29c1c690bf19a993f09
JoonHong <danielkim30433@gmail.com>
2020-08-12 14:00:25 +0900

    맵뷰 현재위치까지

---
+++ app/src/main/java/com/example/wheelwhere/RetrofitService.kt
@@ -0,0 +1,28 @@
+package com.example.wheelwhere
+
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.*
+
+
+interface RetrofitService {
+
+//    @POST("users")
+//    @FormUrlEncoded
+//    fun register(
+//        @Field("id") usernmae: String,
+//        @Field("password") password1: String
+//    ): Call<User>
+//
+//
+//    @POST("users")
+//    @FormUrlEncoded
+//    fun login(
+//        @Field("username") username: String,
+//        @Field("password") password: String
+//    ): Call<User>
+
+
+
+}
