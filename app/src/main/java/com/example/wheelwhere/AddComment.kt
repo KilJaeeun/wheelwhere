4f8a417e34b85325ce3ef336170d93a64ac81c32
KilJaeeun <rha3122@naver.com>
2020-08-17 08:44:43 +0900

    후기  작성 xml

---
+++ app/src/main/java/com/example/wheelwhere/AddComment.kt
@@ -0,0 +1,11 @@
+package com.example.wheelwhere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddComment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empty_commetn)
+    }
+}
