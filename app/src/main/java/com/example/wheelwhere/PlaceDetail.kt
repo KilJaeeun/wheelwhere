948e96a3b6848089e8306a2e89d1e17b60bd779a
KilJaeeun <rha3122@naver.com>
2020-08-17 09:38:15 +0900

    후기 리스트

--- app/src/main/java/com/example/wheelwhere/PlaceDetail.kt
+++ app/src/main/java/com/example/wheelwhere/PlaceDetail.kt
@@ -2,29 +2,44 @@ package com.example.wheelwhere
 
 import android.app.Activity
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_empty_commetn.*
 import kotlinx.android.synthetic.main.activity_place_detail.*
+import kotlinx.android.synthetic.main.activity_place_list.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class PlaceDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place_detail)
         getPlaceInfoAndDraw(this@PlaceDetail)
-
+        NetworkTask2(
+            comments_list,
+            LayoutInflater.from(this@PlaceDetail)
+        ).execute()
 
     }
 
 
-    fun getPlaceInfoAndDraw(activity:Activity) {
+    fun getPlaceInfoAndDraw(activity: Activity) {
         val post_ids = intent.getStringExtra("post")
         val name = intent.getStringExtra("name")
         val address = intent.getStringExtra("address")
@@ -36,40 +51,67 @@ class PlaceDetail : AppCompatActivity() {
             intent.putExtra("post", post_ids)
             activity.startActivity(intent)
         }
+    }
+}
 
+class NetworkTask2(
+    val recyclerView: RecyclerView,
+    val inflater: LayoutInflater
+) : AsyncTask<Any?, Any?, Array<CommentRegister>>() {
+    override fun onPostExecute(result: Array<CommentRegister>) {
+        //다른 실행함수는 서브 앱티브티에서 실행되지ㅏㄴ, 이 친구는 메인쓰레드에서  실행된다.
+
+        val adapter = CommentAdapter(result!!, inflater)
+        recyclerView.adapter = adapter
+        //recyclerView.layoutManager = LinearLayoutManager(context)
+        super.onPostExecute(result)
+    }
+
+    override fun doInBackground(vararg params: Any?): Array<CommentRegister> {
+        val urlString: String = "http://3.35.90.80/comments/"
+        val url: URL = URL(urlString)
+        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        connection.setRequestProperty("Content-Type", "application/json") //헤더
+        var buffer = ""
+        if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+            Log.d("connn", "inputstream: " + connection.inputStream)
+            val reader = BufferedReader(
+                InputStreamReader(
+                    connection.inputStream,
+                    "UTF-8"
+                )
+            )
+            buffer = reader.readLine()
+        }
+        val data = Gson().fromJson(buffer, Array<CommentRegister>::class.java)
+        return data
+    }
+}
+
+class CommentAdapter(
+    val personList: Array<CommentRegister>,
+    val inflater: LayoutInflater
+) : RecyclerView.Adapter<CommentAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val text: TextView
+
+        init {
+            text = itemView.findViewById(R.id.comment_item)
+        }
     }
 
 
-        val retrofit = Retrofit.Builder().baseUrl("http://3.35.90.80")
-            .addConverterFactory(GsonConverterFactory.create()).build()
-        val service = retrofit.create(RetrofitService::class.java)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.comment_item, parent, false)
+        return ViewHolder(view)
+    }
 
+    override fun getItemCount(): Int {
+        return personList.size
+    }
 
-//    service.getComments().enqueue(object : Callback<ArrayList<Place>> {
-//        override fun onFailure(call: Call<ArrayList<Place>>, t: Throwable) {
-//            Log.d("result!!", "Error (getting data)!")
-//        }
-//
-//        override fun onResponse(
-//            call: Call<ArrayList<Place>>,
-//            response: Response<ArrayList<Place>>
-//        ) {
-//            if (response.isSuccessful) {
-//                val dataList = response.body()
-//                Log.d("result!!", "id : " + dataList?.get(0)?.id)
-//                Log.d("result!!", "name : " + dataList?.get(0)?.name)
-//                Log.d("result!!", "is_toilet : " + dataList?.get(0)?.is_toilet)
-//                Log.d("result!!", "is_elibator : " + dataList?.get(0)?.is_elibator)
-//                Log.d("result!!", "is_parking : " + dataList?.get(0)?.is_parking)
-//                Log.d("result!!", "is_helper : " + dataList?.get(0)?.is_helper)
-//                Log.d("result!!", "address : " + dataList?.get(0)?.address)
-//                Log.d("result!!", "is_tuck : " + dataList?.get(0)?.is_tuck)
-//                Log.d("result!!", "description : " + dataList?.get(0)?.description)
-//                Log.d("result!!", "latitude : " + dataList?.get(0)?.latitude)
-//                Log.d("result!!", "longitude : " + dataList?.get(0)?.longitude)
-//                Log.d("result!!", "star : " + dataList?.get(0)?.star)
-//                Log.d("result!!", "author : " + dataList?.get(0)?.author)
-//            }
-//        }
-//    })
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.text.setText(personList.get(position).text ?: "")
+    }
 }
