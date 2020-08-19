f09a4d956192e7eacd184a5b6f2c6ba7c787fb60
KilJaeeun <rha3122@naver.com>
2020-08-19 21:30:42 +0900

    my page 일부 만듦

---
+++ app/src/main/java/com/example/kotlin_tmap/MyPage.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_tmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+    }
+}
