b0a860c03df4c6995ca79cf7466f876c6fbf467f
JoonHong <danielkim30433@gmail.com>
2020-08-24 22:45:44 +0900

    8,10

--- app/src/main/java/com/example/kotlin_tmap/MyPage.kt
+++ app/src/main/java/com/example/kotlin_tmap/MyPage.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MyPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_page)
+        setContentView(R.layout.activity_my_page_t)
     }
 }
