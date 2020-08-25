d524cf030bf2fc6b4c85bff4ce508b990d7b1149
KilJaeeun <rha3122@naver.com>
2020-08-25 13:36:57 +0900

    별점  front 구현

--- app/src/main/java/com/example/kotlin_tmap/AddComment.kt
+++ app/src/main/java/com/example/kotlin_tmap/AddComment.kt
@@ -1,16 +1,13 @@
 package com.example.kotlin_tmap
 
-
 import android.app.Activity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add_comment.*
-import kotlinx.android.synthetic.main.activity_empty_commetn.*
-import kotlinx.android.synthetic.main.activity_empty_commetn.recieve_comment
-import kotlinx.android.synthetic.main.activity_empty_commetn.recieve_comment_btn
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,13 +18,53 @@ class AddComment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_comment)
-        getCommentAndDrawServer(this@AddComment)
+        var flag = 0
+        getCommentAndDrawServer(this@AddComment, 0)
     }
-
-    fun getCommentAndDrawServer(activity:Activity) {
+    fun getCommentAndDrawServer(activity: Activity, flag_input: Int) {
+        var flag = flag_input
         go_back.setOnClickListener {
             super.onBackPressed()
         }
+
+//        // get reference to ImageView
+        var star_input = findViewById(R.id.star_input) as ImageView
+//// set on-click listener for ImageView
+
+        star_input.setOnClickListener {
+            Log.d("star_debug", ""+flag)
+
+                when (flag) {
+                    0 -> {
+                        flag += 1
+                        star_input.setImageResource(R.drawable.star1)
+                    }
+                    1 -> {
+                        flag += 1
+                        star_input.setImageResource(R.drawable.star2)
+                    }
+                    2 -> {
+                        flag += 1
+                        star_input.setImageResource(R.drawable.star3)
+                    }
+                    3 -> {
+                        flag += 1
+                        star_input.setImageResource(R.drawable.star4)
+                    }
+                    4 -> {
+                        flag += 1
+                        star_input.setImageResource(R.drawable.star5)
+                    }
+                    else -> {
+                        flag = 0
+                        star_input.setImageResource(R.drawable.star0)
+                    }
+
+
+                }
+
+        }
+
         val post = intent.getStringExtra("post")
         recieve_comment_btn.setOnClickListener {
             val retrofit = Retrofit.Builder().baseUrl("http://3.35.90.80")
@@ -51,7 +88,7 @@ class AddComment : AppCompatActivity() {
                     response: Response<CommentRegister>
                 ) {
                     if (response.isSuccessful) {
-                        Toast.makeText(activity, "후기작성에 성공하였습니다. "+post, Toast.LENGTH_LONG).show()
+                        Toast.makeText(activity, "후기작성에 성공하였습니다. " + post, Toast.LENGTH_LONG).show()
                         val rest = response.body()
                         Log.d("result!!", "uploaded !")
                         //뒤로가기
