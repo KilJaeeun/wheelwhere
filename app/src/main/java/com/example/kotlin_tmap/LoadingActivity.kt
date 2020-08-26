dea37f136b8590d60f5d4c636ee91e69d73ee50c
KilJaeeun <rha3122@naver.com>
2020-08-26 17:09:40 +0900

    loading activity 구현

---
+++ app/src/main/java/com/example/kotlin_tmap/LoadingActivity.kt
@@ -0,0 +1,25 @@
+package com.example.kotlin_tmap
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.kotlin_tmap.map.MapExample
+
+class LoadingActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 2000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this, MapExample::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+
+}
